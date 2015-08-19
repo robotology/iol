bb7536689459f5004b6c0d488813d573a2fa9cb9
Ugo Pattacini <ugo.pattacini@iit.it>
2015-08-19 18:43:29 +0200

    [iol/mobile]: added "This is $Object" command

--- app/lua/mobile/iol_funcs_mobile.lua
+++ app/lua/mobile/iol_funcs_mobile.lua
@@ -142,6 +142,16 @@ function IOL_what(port)
    return reply:get(0):asString()
 end
 
+function IOL_this_is(port,objName)
+   local wb = yarp.Bottle()
+   local reply = yarp.Bottle()
+   wb:clear()
+    wb:addString("this")
+    wb:addString(objName)
+    port:write(wb,reply)
+   return reply:get(0):asString()
+end
+
 function IOL_calib_kin_start(port, side, objName)
    local wb = yarp.Bottle()
    local reply = yarp.Bottle()
@@ -173,9 +183,9 @@ function string.starts(String,Start)
 end
 
 function splitString(inputstr)
-    
+
     local tmp = yarp.Bottle()
-    
+
     for token in string.gmatch(inputstr, "[^%s]+") do
         tmp:addString(token)
     end
@@ -188,22 +198,22 @@ function Receive_Speech(port)
     str = port:read(false)
     local inString
     if str ~= nil then
-        inString = str:toString()  
-            
+        inString = str:toString()
+
         if string.find(inString, "^%a") then                --check if string needs to be modified
             --do nothing string starts with character
         else
             inString = string.sub(inString, 2, -2)          --remove FIRST and LAST element of string in this case quotation marks
         end
-        
+
         if string.find(inString, "^%l") then
             inString = inString:gsub("^%l", string.upper)   --put a capital letter on the first character of the string
         end
-        
+
         print ("Query is: ", inString)
-        
+
         reply = splitString(inString)
     end
-    
+
     return reply
 end
--- app/lua/mobile/iol_interact_fsm_mobile.lua
+++ app/lua/mobile/iol_interact_fsm_mobile.lua
@@ -12,6 +12,7 @@ event_table = {
    Forget    = "e_forget",
    Explore   = "e_explore",
    What      = "e_what",
+   This      = "e_this",
    Let       = "e_let",
    }
 
@@ -28,16 +29,16 @@ interact_fsm = rfsm.state{
 
             doo = function()
             while true do
-            
+
                 result = Receive_Speech(speechRecog_port)
                 local cmd
                 if result ~= nil then
                     --print("received REPLY: ", result:toString() )
                     cmd = result:get(0):asString()
-                else    
+                else
                     cmd =  "waiting" -- do nothing
                 end
-            
+
                 rfsm.send_events(fsm, event_table[cmd])
                 rfsm.yield(true)
             end
@@ -54,7 +55,7 @@ interact_fsm = rfsm.state{
             rfsm.send_events(fsm, 'e_menu_done')
         end
     },
-   
+
     ----------------------------------
     -- state SUB_CALIBRATE          --
     ----------------------------------
@@ -65,7 +66,7 @@ interact_fsm = rfsm.state{
             speak(ispeak_port,"OK, I know the table height")
     end
     },
-   
+
     ----------------------------------
     -- state SUB_WHERE              --
     ----------------------------------
@@ -75,18 +76,18 @@ interact_fsm = rfsm.state{
             local obj = result:get(3):asString()
             local b = IOL_where_is(iol_port, obj)
             local ret = b:get(0):asString()
-            
+
             if  ret == "ack" or ret == "nack" then
-        
+
                 print("SUB_WHERE : waiting for speech command!")
-                
+
                 repeat
                     reward = Receive_Speech(speechRecog_port)
                 until reward:size() > 0
                 print("reward received REPLY: ", reward:toString() )
-    
+
                 local cmd  =  reward:get(0):asString()
-                
+
                 print("REWARD IS", cmd)
                 if cmd == "Yes" then
                     IOL_reward(iol_port,"ack")
@@ -105,43 +106,43 @@ interact_fsm = rfsm.state{
             end
         end
     },
-   
+
     ----------------------------------
     -- state SUB_TEACH_OBJ          --
     ----------------------------------
 
     SUB_TEACH_OBJ = rfsm.state{
         entry=function()
-            
+
             IOL_track_start(iol_port)
-            
+
             print("SUB_TEACH : waiting for speech command!")
-        
+
             local reward
             repeat
                 reward = Receive_Speech(speechRecog_port)
             until reward:size() > 0
-            
+
             print("reward received REPLY: ", reward:toString() )
-        
+
             local cmd  =  reward:get(0):asString()
-            
+
             print("REPLY IS", cmd)
             if cmd == "There" then
                 IOL_track_stop(iol_port)
                 print("stopped track")
-                
+
                 print("SUB_TEACH_OBJ_NAME : waiting for speech command!")
-                
+
                 speak(ispeak_port,"I will learn the new object, please say its name");
-                
+
                 local str
                 repeat
                     str = Receive_Speech(speechRecog_port)
                 until str:size() > 0
-                
+
                 local objName  =  str:get(3):asString()
-                
+
                 if objName == "ERROR" then
                     speak(ispeak_port,"Skipped");
                 else
@@ -168,58 +169,58 @@ interact_fsm = rfsm.state{
             local b = IOL_take(iol_port, obj)
         end
     },
-   
+
     ----------------------------------
     -- state SUB_GRASP              --
     ----------------------------------
-    
+
     SUB_GRASP = rfsm.state{
         entry=function()
             local obj = result:get(2):asString()
             local b = IOL_grasp(iol_port, obj)
         end
     },
-   
+
     ----------------------------------
     -- state SUB_RETURN             --
     ----------------------------------
-    
+
     SUB_RETURN = rfsm.state{
         entry=function()
             print("going home!")
             IOL_goHome(iol_port)
         end
     },
-    
+
     ----------------------------------
     -- state SUB_TOUCH              --
     ----------------------------------
-    
+
     SUB_TOUCH = rfsm.state{
         entry=function()
             local obj = result:get(2):asString()
             local b = IOL_touch(iol_port, obj)
         end
     },
-   
+
     ----------------------------------
     -- state SUB_PUSH              --
     ----------------------------------
-    
+
     SUB_PUSH = rfsm.state{
         entry=function()
             local obj = result:get(2):asString()
             local b = IOL_push(iol_port, obj)
         end
     },
-    
+
     ----------------------------------
     -- state SUB_FORGET             --
     ----------------------------------
-    
+
     SUB_FORGET = rfsm.state{
         entry=function()
-            local obj 
+            local obj
             obj= result:get(1):asString()
             if obj == "the" then
                 obj= result:get(2):asString()
@@ -227,37 +228,37 @@ interact_fsm = rfsm.state{
             local b = IOL_forget(iol_port, obj)
         end
     },
-    
+
     ----------------------------------
     -- state SUB_EXPLORE            --
-    ----------------------------------   
-    
+    ----------------------------------
+
     SUB_EXPLORE = rfsm.state{
         entry=function()
             local obj = result:get(2):asString()
             local b = IOL_explore(iol_port, obj)
         end
     },
- 
+
     ----------------------------------
     -- state SUB_WHAT               --
-    ----------------------------------  
-    
+    ----------------------------------
+
     SUB_WHAT = rfsm.state{
     entry=function()
         local answer = IOL_what(iol_port)
-    
+
             if  answer == "ack" then
-    
+
                 print("SUB_WHAT : waiting for speech command!")
-    
+
                 repeat
                     reward = Receive_Speech(speechRecog_port)
                 until reward:size() > 0
-    
+
                 print("What received REPLY: ", reward:toString() )
                 local cmd  =  reward:get(0):asString()
-                local obj  =  reward:get(3):asString() 
+                local obj  =  reward:get(3):asString()
                 print("REWARD IS", cmd)
                 if cmd == "Yes" then
                     print("YES", obj)
@@ -275,13 +276,13 @@ interact_fsm = rfsm.state{
                     speak(ispeak_port,"I don't understand")
                 end
             elseif answer == "nack" then
-                
+
                 print("SUB_WHAT : waiting for speech command!")
-                
+
                 repeat
                     reward = Receive_Speech(speechRecog_port)
                 until reward:size() > 0
-                
+
                 local cmd  =  reward:get(0):asString()
                 local obj  =  reward:get(3):asString()
                 if cmd == "Skip" then
@@ -296,17 +297,29 @@ interact_fsm = rfsm.state{
             end
         end
     },
-   
+
+    ----------------------------------
+    -- state SUB_THIS               --
+    ----------------------------------
+
+    SUB_THIS = rfsm.state{
+            entry=function()
+                    local obj = result:get(7):asString()
+
+                    local b = IOL_this_is(iol_port, obj)
+            end
+    },
+
     ----------------------------------
     -- state SUB_LET                --
-    ----------------------------------    
-    
+    ----------------------------------
+
     SUB_LET = rfsm.state{
     entry=function()
         let_obj = result:get(8):asString()
         let_arm = result:get(11):asString()
         speak(ispeak_port,"Do you mean show me how to reach the " .. let_obj .. " with my " .. let_arm .." arm? ")
-        
+
         print("SUB_LET : waiting for speech command!")
         repeat
             reward = Receive_Speech(speechRecog_port)
@@ -314,7 +327,7 @@ interact_fsm = rfsm.state{
 
         let_cmd  =  reward:get(0):asString()
     end,
-    
+
     doo = function()
         while let_cmd == "Yes" do
             print("Enter teaching mode")
@@ -329,7 +342,7 @@ interact_fsm = rfsm.state{
                 repeat
                     fin = Receive_Speech(speechRecog_port)
                 until reward:size() > 0
-                
+
                 local cmd  =  fin:get(0):asString()
                 if cmd == "No" then
                     let_cmd = "done"
@@ -344,19 +357,19 @@ interact_fsm = rfsm.state{
 
     ----------------------------------
     -- state SUB_KIN                --
-    ---------------------------------- 
-    
+    ----------------------------------
+
     SUB_KIN = rfsm.state{
         doo = function()
             local finish = false
             while not finish do
-            
+
                 print("SUB_KIN : waiting for speech command!")
                 local fin
                 repeat
                     fin = Receive_Speech(speechRecog_port)
                 until reward:size() > 0
-            
+
                 local cmd  =  fin:get(0):asString()
                 if cmd == "Finished" then
                     IOL_calib_kin_stop(iol_port)
@@ -370,46 +383,49 @@ interact_fsm = rfsm.state{
     ----------------------------------
     -- state transitions            --
     ----------------------------------
-    
+
     rfsm.trans{ src='initial', tgt='SUB_MENU'},
     rfsm.transition { src='SUB_MENU', tgt='SUB_EXIT', events={ 'e_exit' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_CALIBRATE', events={ 'e_calibrate' } },
     rfsm.transition { src='SUB_CALIBRATE', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_WHERE', events={ 'e_where' } },
     rfsm.transition { src='SUB_WHERE', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_TEACH_OBJ', events={ 'e_i_will' } },
     rfsm.transition { src='SUB_TEACH_OBJ', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_TAKE', events={ 'e_take' } },
     rfsm.transition { src='SUB_TAKE', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_GRASP', events={ 'e_grasp' } },
     rfsm.transition { src='SUB_GRASP', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_RETURN', events={ 'e_home' } },
     rfsm.transition { src='SUB_RETURN', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_TOUCH', events={ 'e_touch' } },
     rfsm.transition { src='SUB_TOUCH', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_PUSH', events={ 'e_push' } },
     rfsm.transition { src='SUB_PUSH', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_FORGET', events={ 'e_forget' } },
     rfsm.transition { src='SUB_FORGET', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_EXPLORE', events={ 'e_explore' } },
     rfsm.transition { src='SUB_EXPLORE', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_WHAT', events={ 'e_what' } },
     rfsm.transition { src='SUB_WHAT', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
+    rfsm.transition { src='SUB_MENU', tgt='SUB_THIS', events={ 'e_this' } },
+    rfsm.transition { src='SUB_THIS', tgt='SUB_MENU', events={ 'e_done' } },
+
     rfsm.transition { src='SUB_MENU', tgt='SUB_LET', events={ 'e_let' } },
     rfsm.transition { src='SUB_LET', tgt='SUB_MENU', events={ 'e_done' } },
-    
+
     rfsm.transition { src='SUB_LET', tgt='SUB_KIN', events={ 'e_kin' } },
     rfsm.transition { src='SUB_KIN', tgt='SUB_MENU', events={ 'e_done' } },
 
